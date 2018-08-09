001dd91383d0d5f20ec6dc5303cb9504f73dacc5
Vinyl Darkscratch <vinyldarkscratch@gmail.com>
2018-08-08 21:28:14 -0700

    Rearrange functions

--- modules/plugin_simple_seo/seo.py
+++ modules/plugin_simple_seo/seo.py
@@ -6,9 +6,43 @@ __author__ = ['cccaballero', 'vinyldarkscratch']
 from gluon import *
 from collections import OrderedDict
 
-# -=- Initialize -=-
+# -=- Basic Set Functions -=-
 
-def init_seo(type="website", card="summary", title=None, author=None, keywords=None, generator="Web2py Web Framework", image=None, description=None, site_name=None, locale=None, locale_alternate={}, twitter_username=None, label1=None, data1=None, label2=None, data2=None):
+# web2py Meta
+def set_meta(name, value):
+	current.response[name] = value
+
+# Open Graph
+def set_og(name, value):
+	if isinstance(value, list):
+		for count in range(len(value)):
+			d = OrderedDict()
+			d['property'] = "og:%s" %name
+			d['content'] = value[count]
+			current.response.meta['og_%s_%d' %(name, count)] = d
+	else:
+		d = OrderedDict()
+		d['property'] = "og:%s" %name
+		d['content'] = value
+		current.response.meta['og_%s' %name] = d
+
+# Twitter Card
+def set_tc(name, value):
+	if isinstance(value, list):
+		for count in range(len(value)):
+			d = OrderedDict()
+			d['property'] = "twitter:%s" %name
+			d['content'] = value[count]
+			current.response.meta['tc_%s_%d' %(name, count)] = d
+	else:
+		d = OrderedDict()
+		d['property'] = "twitter:%s" %name
+		d['content'] = value
+		current.response.meta['tc_%s' %name] = d
+
+# -=- Initializers -=-
+
+def init_seo(type="website", card="summary", title=None, author=None, keywords=None, generator="Web2py Web Framework", image=None, description=None, site_name=None, locale=T.accepted_language or 'en', locale_alternate=[], twitter_username=None, label1=None, data1=None, label2=None, data2=None):
 	init_meta(title, description, keywords, author, generator)
 	init_og(type, title, image, description, site_name, locale, locale_alternate)
 	init_tc(card, title, twitter_username, label1, data1, label2, data2, image, description)
@@ -18,7 +52,7 @@ def init_meta(title=None, description=None, keywords=None, author=None, generato
 	for name in ['title', 'description', 'keywords', 'author', 'generator']:
 		if data[name]: set_meta(name, data[name])
 
-def init_og(type="website", title=None, image=None, description=None, site_name=None, locale=None, locale_alternate={}):
+def init_og(type="website", title=None, image=None, description=None, site_name=None, locale=None, locale_alternate=[]):
 	url = URL(args=current.request.args, host=True)
 	data = locals()
 	for name in ['type', 'title', 'image', 'url', 'description', 'site_name', 'locale']:
@@ -68,35 +102,3 @@ def description(new_description):
 def set_image(image):
 	set_og("image", image)
 	set_tc("image", image)
-
-# web2py Meta
-def set_meta(name, value):
-	current.response[name] = value
-
-# Open Graph
-def set_og(name, value):
-	if isinstance(value, list):
-		for count in range(len(value)):
-			d = OrderedDict()
-			d['property'] = "og:%s" %name
-			d['content'] = value[count]
-			current.response.meta['og_%s_%d' %(name, count)] = d
-	else:
-		d = OrderedDict()
-		d['property'] = "og:%s" %name
-		d['content'] = value
-		current.response.meta['og_%s' %name] = d
-
-# Twitter Card
-def set_tc(name, value):
-	if isinstance(value, list):
-		for count in range(len(value)):
-			d = OrderedDict()
-			d['property'] = "twitter:%s" %name
-			d['content'] = value[count]
-			current.response.meta['tc_%s_%d' %(name, count)] = d
-	else:
-		d = OrderedDict()
-		d['property'] = "twitter:%s" %name
-		d['content'] = value
-		current.response.meta['tc_%s' %name] = d
